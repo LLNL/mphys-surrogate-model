52e2f80621d2e4326b87d897dc457b7316c2b3a6
Nipun Gunawardena <gunawardena1@llnl.gov>
2025-07-03 11:31:37 -0700

    Added sequential testing

--- ng_scripts/Random_Seed_Variation/random_seed_variation.py
+++ ng_scripts/Random_Seed_Variation/random_seed_variation.py
@@ -34,9 +34,90 @@ else:
     raise NotImplementedError(f"Model type {MODEL_TYPE} is not implemented")
 
 
+def train_model(args):
+    # Unpack args
+    random_seed, n_bins, train_loader, test_loader = args
+
+    # Set seed
+    torch.manual_seed(random_seed)
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+
+    # Initialize the model
+    if MODEL_TYPE == "AE-AR":
+        model = AEAutoregressor(
+            n_channels=1,
+            n_bins=n_bins,
+            n_latent=params["latent_dim"],
+            n_lag=params["n_lag"],
+            CNN=params["CNN"],
+        )
+    elif MODEL_TYPE == "NNdzdt":
+        model = AENNdzdt(
+            n_channels=1,
+            n_bins=n_bins,
+            n_latent=params["latent_dim"],
+            layer_size=params["layer_size"],
+            CNN=params["CNN"],
+        )
+    elif MODEL_TYPE == "AE-SINDy":
+        model = AESINDy(
+            n_channels=1,
+            n_bins=n_bins,
+            n_latent=params["latent_dim"],
+            poly_order=params["poly_order"],
+            CNN=params["CNN"],
+            sequential_thresholding=(
+                True
+                if params["sequential_thresholding_interval"] is not None
+                else False
+            ),
+        )
+        if params["sequential_threshold_method"] is not None:
+            # For now, code just has to be copied from train_ae_sindy.py because
+            # initializing thresholder requires access to model
+            params["thresholder"] = thresholding.AdaptiveSequentialThresholdingSINDy(
+                model.dzdt,
+                thresholding.AdaptiveThresholdAnalyzer(
+                    method=params["sequential_threshold_method"],
+                    min_epochs_between=params["sequential_thresholding_interval"],
+                ),
+            )
+    else:
+        raise NotImplementedError(f"Model type {MODEL_TYPE} is not implemented")
+
+    # Optimizer and scheduler
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=params["learning_rate"], weight_decay=params["wd"]
+    )
+    sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min")
+
+    # Training loop
+    num_epochs = 10
+    train_output = train_and_eval(
+        num_epochs,
+        model,
+        train_loader,
+        test_loader,
+        optimizer,
+        sched,
+        params,
+        early_stopping=None,
+        print_flag=False,
+        optuna_trial=None,
+    )
+    losses = train_output[1]
+    best_train_loss = np.min(losses)
+
+    # Print
+    print(f"Best train loss for seed {random_seed}: {best_train_loss}")
+
+    return best_train_loss
+
+
 if __name__ == "__main__":
-    total_trials = 48  # On mac with 8 perf. cores, choose multiple of 8 total_trials
-    parallel_flag = True
+    total_trials = 8  # On mac with 8 perf. cores, choose multiple of 8 total_trials
+    parallel_flag = False
 
     # Open dataset
     if params["data_src"] == "box":
@@ -71,6 +152,13 @@ if __name__ == "__main__":
         test_data = du.NormedBinDatasetDzDt(x_test, dsd_time, m_test)
     else:
         raise NotImplementedError(f"Model type {MODEL_TYPE} is not implemented")
+    batch_size = 8
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=batch_size, shuffle=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_data, batch_size=len(test_data), shuffle=True
+    )
 
     # Set weights
     if MODEL_TYPE == "NNdzdt" or MODEL_TYPE == "AE-SINDy":
@@ -96,3 +184,13 @@ if __name__ == "__main__":
         if total_trials % n_workers:
             raise RuntimeError("Ensure total trials is a multiple of n_workers")
     trials_per_worker = int(total_trials / n_workers)
+
+    # Run study
+    start_time = time.time()
+    if parallel_flag:
+        pass
+    else:
+        res = np.zeros(total_trials)
+        for i in range(total_trials):
+            args = (i, n_bins, train_loader, test_loader)
+            res[i] = train_model(args)
